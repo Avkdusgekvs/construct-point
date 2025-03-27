9063d47f3fe51bfd9682c9cdb18a6c7549edf89a
abhisek <beheraabhisek831@gmail.com>
2025-03-27 22:17:24 +0530

    Fix react-scripts permission issue

--- build/src/components/pages/Rentals.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Rentals = () => {
-  return <h1>Rental Services</h1>;
-};
-
-export default Rentals;
--- build/src/components/pages/Workers.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Workers = () => {
-  return <h1>Construction Workers Listings</h1>;
-};
-
-export default Workers;
